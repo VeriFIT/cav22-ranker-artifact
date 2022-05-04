c3fd916b3c7a11dff46636ce22a1e9d4b0a91dfd
Ondra Lengal <ondra.lengal@gmail.com>
2022-05-04 18:05:47 +0200

    some experiment file

---
+++ ba-compl-eval/run_experiments_full.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check the number of command-line arguments
+if [ \( "$#" -lt 1 \) ] ; then
+	echo "This script will run all experiments, including the tools that are not"
+	echo "in the graphs in the paper (expect long running time!!)"
+	echo
+	echo "usage: ${0} <input-set>"
+	echo
+	echo "   with <input-set> in {from_ltl_red, advanced-automata, random-all-compact}"
+	exit 1
+fi
+
+INPUT="$1"
+
+# timeout in seconds
+TIMEOUT=60
+
+BIN_DIR="$(pwd)/bin"
+INPUT_FILE="${INPUT}.input"
+TASK_FILE="../${INPUT}-to${TIMEOUT}.tasks"
+
+export LD_LIBRARY_PATH="${BIN_DIR}"
+
+cd bench
+cat from_ltl_red.input | ./pycobench -t ${TIMEOUT} -o ${TASK_FILE} ba-compl.yaml
